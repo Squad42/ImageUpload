b91a7f9f189ce8985379682808fe64973bc2ea8e
maticb <matic.bernik@marand.si>
2019-11-14 20:40:09 +0100

    first test

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,11 +1,14 @@
 import pytest
+from urllib import request
 from imageUpload.server import *
+from imageUpload.server_views import upload_file_to_s3
 
+#class Test_Amazon_S3:
+	
+def test_imageUpload():
+	with open('tests/test_resouces/test.jpg','rb') as upload_file:
+		upload_file.filename='test.jpg'
+		upload_file.content_type='image/jpg'
+		s3_file_url = upload_file_to_s3(upload_file, app.config["S3_BUCKET"])
+	assert "amazonaws.com/test.jpg" in s3_file_url
 
-# content of test_sample.py
-def inc(x):
-    return x + 2
-
-
-def test_answer():
-    assert inc(3) == 5
